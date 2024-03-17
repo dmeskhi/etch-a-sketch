c3e1cc786b93eda81fe1b01bf8f186498b9516c4
David Meskhishvili <dmeskh@gmail.com>
2024-03-16 17:15:37 -0700

    Correct issue concerning improper display

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-let board = document.querySelector(".board");
-board.style.gridTemplateColumns = "repeat(16, 1fr)";
-board.style.gridTemplateRows = "repeat(16, 1fr)";
+function createBoard(size) {
+  let board = document.querySelector(".board");
+  let squares = document.querySelectorAll("div");
+  squares.forEach((div) => div.remove());
+  
+  board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-for (let i = 0; i < 256; i++) {
+  for (let i = 0; i < 256; i++) {
     let square = document.createElement("div");
     square.style.backgroundColor = "blue";
     board.insertAdjacentElement("beforeend", square);
+  }
+}
+createBoard(16);
+
+function changeSize(input){
+    createBoard(input);
 }
