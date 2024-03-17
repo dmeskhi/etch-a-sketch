3588735683c2f9785db96736c6efd233038d7301
David Meskhishvili <dmeskh@gmail.com>
2024-03-16 18:19:13 -0700

    Finish project

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
 //global variable
 let color = "black";
 
+//Create sketchBoard
 function createBoard(size) {
   let board = document.querySelector(".board");
+  //avoid problems when getting higher value than 16
   let squares = board.querySelectorAll("div");
   squares.forEach((div) => div.remove());
   board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
   board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
+  //determine any value
   let amount = size * size;
+  //Create squares
   for (let i = 0; i < amount; i++) {
     let square = document.createElement("div");
     square.addEventListener("mouseover", colorDot);
     square.style.backgroundColor = "white";
+    //New method for the developer. It inserts a given 
+    //element node at a given position relative to 
+    //the element it is invoked upon
     board.insertAdjacentElement("beforeend", square);
   }
 }
